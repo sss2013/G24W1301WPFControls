da6b2db1f4f99339be6dab3a623f2b5217b9ec33
sss2013 <kshi9478@gmail.com>
2024-11-28 15:50:17 +0900

    초기 커밋.

---
+++ G24W1301WPFControls/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace G24W1301WPFControls
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnAdd(object sender, RoutedEventArgs e)
+        {
+            string str = $"{Singer.Text}{(HasJongsung(Singer.Text) ? "이" : "가")} 부른 "
+                + $"{Genre.Text} 장르의 "
+                + $"{Title.Text}{(HasJongsung(Title.Text) ? "이" : "가")} 추가됨\n";
+            //Result.Text += str;
+            Result.Text = str + Result.Text;
+        }
+        public bool HasJongsung(string str)
+        {
+            if (str.Length < 1)
+                return true;
+            char last = str[str.Length - 1];
+            return (last - 44032) % 28 != 0 ? true : false;
+        }
+    }
+}
